3f0c1813b16abc06025b6775ac9bd7667d276f21
wertrain <wertrain@gmail.com>
2016-11-11 00:42:05 +0900

    WIP

---
+++ Assets/GameMain.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMain : MonoBehaviour {
+
+    private MessageEngine messageEngine;
+
+    // Use this for initialization
+    void Start ()
+    {
+        UnityEngine.UI.Text uiText = GetComponentInChildren<UnityEngine.UI.Text>();
+        messageEngine = new MessageEngine(uiText);
+        messageEngine.SetMessage(
+            "これはテストメッセージです。現在試作中のため、動作がおかしい場合があります。このエンジンでは指定行で勝手に改行されます。\n"
+            + "メッセージで￥nを入力すれば、手動での改行も可能です。改善の余地がかなりあります。\n"
+            + "リッチテキストには未対応です。試しに使ってみると、こんなことになります。<b>太字</b> や<color=yellow>色変え</color>！！！\n"
+        );
+    }
+	
+    // Update is called once per frame
+    void Update ()
+    {
+        messageEngine.Update();
+    }
+}
---
+++ Assets/MessageEngine.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MessageEngine
+{
+    private UnityEngine.UI.Text messageUI;
+    private string allMessage;
+    private string currentMessage;
+    private int messageStartIndex;
+    private int messageEndIndex;
+
+    private int maxTextLine;
+    private int maxTextInLine;
+    private int maxTextLengthInPage;
+
+    private int currentIndex;
+    private bool messageExit;
+
+    public MessageEngine(UnityEngine.UI.Text uiText)
+    {
+        messageUI = uiText;
+        messageStartIndex = 0;
+        messageEndIndex = 0;
+        maxTextLine = 4;
+        maxTextInLine = 28;
+        maxTextLengthInPage = maxTextInLine * maxTextLine;
+        currentIndex = 0;
+        currentMessage = string.Empty;
+        messageExit = false;
+    }
+
+    public void SetMessage(string message)
+    {
+        allMessage = message;
+    }
+
+    public void Update()
+    {
+        if (messageUI == null) return;
+        if (messageExit) return;
+
+        if (currentMessage.Length == 0)
+        {
+            currentMessage = allMessage.Substring(messageStartIndex, Mathf.Min(maxTextLengthInPage, allMessage.Length - messageStartIndex));
+            char[] currentMessageCharArray = currentMessage.ToCharArray();
+            currentMessage = string.Empty;
+
+            int lineCount = 0;
+            bool exit = false;
+            for (int i = 0; i < currentMessageCharArray.Length; ++i)
+            {
+                char c = currentMessageCharArray[i];
+                currentMessage += c;
+                if (++lineCount > maxTextInLine)
+                {
+                    currentMessage += '\n';
+                    lineCount = 0;
+                }
+                switch (c)
+                {
+                    case '\n':
+                        lineCount = 0;
+                        if (++lineCount >= maxTextLine)
+                            exit = true;
+                        break;
+                }
+                messageEndIndex = messageStartIndex + i;
+                if (exit)
+                {
+                    break;
+                }
+            }
+        }
+
+        if (++currentIndex >= currentMessage.Length)
+        {
+            currentMessage = string.Empty;
+            messageStartIndex = messageEndIndex;
+            if (messageStartIndex >= allMessage.Length)
+                messageExit = true;
+            currentIndex = 0;
+        }
+        else
+        {
+            messageUI.text = currentMessage.Substring(0, currentIndex);
+        }
+    }
+}

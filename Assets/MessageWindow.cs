2ce79fafd347a34f637dda82173358d8fb9ba647
wertrain <wertrain@gmail.com>
2016-11-14 19:45:12 +0900

    add MessageWindow class

---
+++ Assets/MessageWindow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * 禁則処理と自動開業に対応したメッセージウィンドウクラス
+ **/
+public class MessageWindow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
